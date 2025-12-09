fceade8a971a120c1b9f2e503422bf1682b05c1f
Awarix <just_tevy@mail.ru>
2025-12-09 15:12:56 +0300

    fix

--- my-minikit-app/next.config.ts
+++ my-minikit-app/next.config.ts
@@ -15,10 +15,6 @@ const nextConfig: NextConfig = {
             key: "X-Frame-Options",
             value: "ALLOWALL",
           },
-          {
-            key: "Content-Security-Policy",
-            value: "frame-ancestors 'self' https://*.farcaster.xyz https://*.warpcast.com https://base.org https://*.base.org https://*.base.dev https://base.dev;",
-          },
         ],
       },
     ];
